bf9008e9bfea3a11592b96c8880258465d9b486b
Daniel Vena <deaconvena@gmail.com>
2020-08-31 14:25:24 -0400

    forgotten debugging

--- templates/podcasts/taxonomy-header.php
+++ templates/podcasts/taxonomy-header.php
@@ -18,7 +18,6 @@ if (!defined('ABSPATH')) die('No direct access allowed');
 </header>
 
 <?php $current_term = \get_queried_object() ?>
-<?php var_dump($current_term); ?>
 <?php if ($current_term && $current_term->taxonomy !== 'wpn_podcast_tag'): ?>
     <?php $tax_tags = get_category_tags($current_term, 'wpn_podcast_tag') ?>
     <?php if (count($tax_tags)): ?>
