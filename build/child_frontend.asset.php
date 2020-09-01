32736741118df6211586e4f7dca923492b971a6d
Daniel Vena <deaconvena@gmail.com>
2020-09-01 19:32:38 -0400

    minor spacing adjustment

--- build/child_frontend.asset.php
+++ build/child_frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '93a1ed8661519a1cba12912f64fc5267');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '77f9f0572374b324e0b940b74d418d8f');
